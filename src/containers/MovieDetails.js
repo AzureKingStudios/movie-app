2920213eb3fd433e3f739a6c8dac926a810c23f0
azure desktop <aden7x@gmail.com>
2019-05-14 19:58:39 -0700

    Add image and movie info to movie details

--- src/containers/MovieDetails.js
+++ src/containers/MovieDetails.js
@@ -19,28 +19,38 @@ class MovieDetails extends Component {
 
     render() {
         
+        let movie = this.state.movie;
+        console.log('movie',movie)
         
         let qs = this.props.location.search;
         qs = qs.replace('?', '');
         //qs is parsed to an int so that movie id and qs can be compared
         qs = parseInt(qs);
         //checks to see if the query string has been updated
-        if(this.state.movie.id !== qs) {
+        if(movie.id !== qs) {
             this.getMovie(qs);
         }
         
-        console.log('state2',this.state.movie);
-        
-        if(typeof this.state.movie.id === 'undefined') {
+        if(typeof movie.id === 'undefined') {
             return(
                 <div>nothing here yet</div>
             )
         }
 
+        let image = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
         return(
             <div>
                 <Header/>
-                movie details
+                <div className='movie-details'>
+                    <img className='movie-detail-img' src={image} alt={movie.title}/>
+                    <div className='movie-info'>
+                        <h2>{movie.title}</h2>
+                        <p>{movie.overview}</p>
+                        <p>Runtime: {movie.runtime} minutes</p>
+                        <p>Release Date: {movie.release_date}</p>
+                    </div>
+                </div>
             </div>
         )
     }
